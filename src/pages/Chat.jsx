ff61ec5a5e4512468f965d7cbf7f879e39f3dc8e
Tejas Weldode <112164797+Tejas-Weldode@users.noreply.github.com>
2024-07-17 11:32:16 +0530

    check otherUser is not null

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -17,22 +17,6 @@ export default function Chat() {
     const messagesEndRef = useRef(null);
 
     useEffect(() => {
-        console.log(otherUser);
-    }, []);
-
-    function formatTimestamp(timestamp) {
-        const date = new Date(timestamp);
-        const options = {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-            hour: "2-digit",
-            minute: "2-digit",
-        };
-        return `${date.toLocaleTimeString(undefined, options)}`;
-    }
-
-    useEffect(() => {
         const fetchMessages = async () => {
             try {
                 setLoading(true);
@@ -64,7 +48,7 @@ export default function Chat() {
                 setLoading(false);
             } catch (error) {
                 toast.error(
-                    error.response?.data?.error || "Error fetching messages"
+                    error.response?.data?.error || "Error fetching user info"
                 );
                 setLoading(false);
             }
@@ -126,21 +110,22 @@ export default function Chat() {
             <div className="flex items-center">
                 {/* image (profilePic) div below --- start*/}
                 <div className="overflow-hidden size-10 rounded-full">
-                    {otherUser.profilePic == null ||
-                    otherUser.profilePic == "" ? (
-                        ""
-                    ) : (
+                    {otherUser?.profilePic ? (
                         <img
                             className="w-full h-full object-cover"
                             src={otherUser.profilePic}
                         />
+                    ) : (
+                        ""
                     )}
                 </div>
                 {/* image (profilePic) div below --- end*/}
                 <p className="font-semibold text-2xl mx-2">
-                    {otherUser.fullName}
+                    {otherUser?.fullName}
                 </p>
-                <p className="font-light italic mx-2">({otherUser.username})</p>
+                <p className="font-light italic mx-2">
+                    ({otherUser?.username})
+                </p>
             </div>
             <div className="overflow-y-scroll max-h-[50vh]">
                 {messages.map((message, index) => (
@@ -160,17 +145,14 @@ export default function Chat() {
                             } `}
                         >
                             {/* image (profilePic) div below --- start*/}
-                            {
-                                <img
-                                    className="w-full h-full object-cover"
-                                    src={
-                                        message.senderId === id
-                                            ? otherUser.profilePic
-                                            : userData.profilePic
-                                    }
-                                />
-                            }
-
+                            <img
+                                className="w-full h-full object-cover"
+                                src={
+                                    message.senderId === id
+                                        ? otherUser?.profilePic || ""
+                                        : userData.profilePic
+                                }
+                            />
                             {/* image (profilePic) div below --- end*/}
                         </div>
                         <div
