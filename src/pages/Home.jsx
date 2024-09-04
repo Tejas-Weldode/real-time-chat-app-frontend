156e3ba121332f4ebd5c2ab4ee4263aa9dfbb950
Tejas Weldode <112164797+Tejas-Weldode@users.noreply.github.com>
2024-09-04 07:24:04 +0530

    redesigned the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { React } from "react";
 import { useAuthContext } from "../context/AuthContext";
+import { Link } from "react-router-dom";
 
 export default function Home() {
     const { userData } = useAuthContext();
@@ -8,12 +9,37 @@ export default function Home() {
         <>
             {userData ? (
                 <div>
-                    <h1>Home</h1>
-                    <p>Hello {userData.fullName}</p>
+                    <h1>Hello {userData.fullName}</h1>
+                    <Link
+                        className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded"
+                        to="/chat"
+                    >
+                        Chat
+                    </Link>
+                    <Link
+                        className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded"
+                        to="/chat/discover-people"
+                    >
+                        Discover People
+                    </Link>
                     <br />
                 </div>
             ) : (
-                <h1>Home</h1>
+                <>
+                    <h1>Welcome to the Chat App</h1>
+                    <Link
+                        className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded"
+                        to="/login"
+                    >
+                        Login
+                    </Link>
+                    <Link
+                        className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded"
+                        to="/signup"
+                    >
+                        Signup
+                    </Link>
+                </>
             )}
         </>
     );
